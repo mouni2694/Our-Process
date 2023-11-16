081b9e759814c646c757e69b248b8eaf107fa59d
Mounica Laveti <krishnamounicalaveti@gmail.com>
2023-11-16 22:00:19 +0100

    created our process block

---
+++ src/App.js
@@ -0,0 +1,7 @@
+
+import './App.css';
+import './components/our-process/our-process'
+import OurProcess from './components/our-process/our-process';
+
+const App = () => <OurProcess/>
+export default App;
---
+++ src/components/our-process/our-process.js
@@ -0,0 +1,111 @@
+import "./our-process.css";
+const OurProcess = (props) => {
+    return (
+        <div class = "container">
+        <h1 class = "heading">OUR <span class = "boldWord">PROCESS</span></h1>
+        <div  class = "row">
+          <div class = "col-lg">
+            <div>
+          <h1 class = "serialNumber">01</h1>
+            <div class="card-body">
+          <h5 class="card-title">ON BOARDING</h5><br></br>
+          <p class="card-text">A simple form to fill in and get into the success path</p>
+    
+            </div>
+            </div>
+
+
+          </div>
+
+
+          <div class = "col-lg">
+            <div>
+          <h1 class = "serialNumber">02</h1>
+            <div class="card-body"> 
+          <h5 class="card-title">ASSESSMENTS</h5><br></br>
+          <p class="card-text">With our orientation tools we assess the student and set the path</p>
+    
+            </div>
+            </div>
+
+
+          </div>
+
+
+          <div class = "col-lg">
+            <div>
+          <h1 class = "serialNumber">03</h1>
+            <div class="card-body">
+          <h5 class="card-title">PLAN</h5><br></br>
+          <p class="card-text">Details orientation plan on how the student is going to take the sessions</p>
+    
+            </div>
+            </div>
+
+
+          </div>
+
+          
+
+
+
+
+          
+        </div>
+
+        <div class = "row">
+
+          
+        <div class = "col-lg">
+            <div>
+          <h1 class = "serialNumber">04</h1>
+            <div class="card-body">
+          <h5 class="card-title">EXECUTION</h5><br></br>
+          <p class="card-text">With real time projects students will be executing the work they are learning</p>
+    
+            </div>
+            </div>
+
+
+          </div>
+
+
+
+
+          <div class = "col-lg">
+            <div>
+          <h1 class = "serialNumber">05</h1>
+            <div class="card-body">
+          <h5 class="card-title">SUCCESSFUL</h5><br></br>
+          <p class="card-text">Plan execution will be successful students at the end of their course plan</p>
+    
+            </div>
+            </div>
+
+
+          </div>
+
+
+          <div class = "col-lg">
+            <div>
+          <h1 class = "serialNumber">06</h1>
+            <div class="card-body">
+          <h5 class="card-title">CERTICATION</h5><br></br>
+          <p class="card-text">Interested students will go thru the certification programs to build the solid path</p>
+    
+            </div>
+            </div>
+
+
+          </div>
+        </div>
+    </div>
+    
+
+    
+    
+
+    );
+  }
+
+  export default OurProcess;
